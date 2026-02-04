8d3c0b281ba8042d8bfb8f4e2e5059f4ad0cdf4d
jzzmined <j.cadelina.554273@umindanao.edu.ph>
2026-02-04 23:26:57 +0700

    feat: implement calculator functionality and update HTML structure

---
+++ calculator_task8.js
@@ -0,0 +1,18 @@
+
+
+const screen = document.getElementById("screen");
+
+function appendToDisplay(input) {
+        screen.value += input;
+        screen.value * input;
+        screen.value - input;
+        screen.value / input;
+}
+
+function clearScreen() {
+        screen.value = "";
+}
+
+function calculate() {
+        screen.value = eval(screen.value);
+}
