191db27f6cbfbe435fdd0166490a74adbb06c663
jzzmined <j.cadelina.554273@umindanao.edu.ph>
2026-02-05 14:21:09 +0700

    refactor: update button label and enhance keyboard input handling in JS

--- calculator_task8.js
+++ calculator_task8.js
@@ -1,26 +1,61 @@
 
+document.addEventListener('DOMContentLoaded', function() {
+    const screen = document.getElementById("screen");
 
-const screen = document.getElementById("screen");
+    function appendToDisplay(input) {
+            screen.value += input;
+    }
 
-function appendToDisplay(input) {
-        screen.value += input;
-        screen.value * input;
-        screen.value - input;
-        screen.value / input;
-}
-
-function clearScreen() {
+    function clearScreen() {
         screen.value = "";
-}
+    }
 
-function calculate() {
+    function calculate() {
     try {
         screen.value = eval(screen.value);
     } catch (error) {
         screen.value = "Syntax Error";
     }
-}
+    }
 
-function deleteOn() {
+    function deleteOn() {
         screen.value = screen.value.slice(0, -1);
-}       
+    }  
+
+    window.appendToDisplay = appendToDisplay;
+    window.clearScreen = clearScreen;
+    window.calculate = calculate;
+    window.deleteOn = deleteOn;
+
+    document.addEventListener('keydown', function(event) {
+        console.log("Key pressed:", event.key)
+        const key = event.key;
+
+        if (key >= '0' && key <= '9') {
+            handleInput(key);
+        }
+
+        else if (['+', '-', '*', '/'].includes(key)) {
+            handleInput(key);
+        }
+
+        else if (key == 'Enter') {
+            event.preventDefault()
+            calculate();
+        }
+
+        else if (key == 'Backspace') {
+            handleBackspace();
+            deleteOn();
+        }
+
+        else if (key === 'Escape' || key === 'c'  || key === 'C') {
+            clearScreen();
+        }
+
+        else if (key === '.') {
+            handleInput(key);
+        }
+    });
+})
+
