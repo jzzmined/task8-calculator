bee4c77e43c0c39f11898212f25bcacd0fd2560b
jzzmined <j.cadelina.554273@umindanao.edu.ph>
2026-02-05 15:24:23 +0700

    Reconnected repository

---
+++ calculator_task8.js
@@ -0,0 +1,62 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const screen = document.getElementById("screen");
+
+    function appendToDisplay(input) {
+            screen.value += input;
+    }
+
+    function clearScreen() {
+        screen.value = "";
+    }
+
+    function calculate() {
+        console.log("i love xyz");
+    // try {
+    //     screen.value = eval("i love u xyz");
+    // } catch (error) {
+    //     screen.value = "Syntax Error";
+    // }
+    }
+
+    function deleteOn() {
+        screen.value = screen.value.slice(0, -1);
+    }  
+
+    window.appendToDisplay = appendToDisplay;
+    window.clearScreen = clearScreen;
+    window.calculate = calculate;
+    window.deleteOn = deleteOn;
+
+    document.addEventListener('keydown', function(event) {
+        console.log("Key pressed:", event.key)
+        const key = event.key;
+
+        if (key >= '0' && key <= '9') {
+            appendToDisplay(key);
+        }
+
+        else if (['+', '-', '*', '/'].includes(key)) {
+            appendToDisplay(key);
+        }
+
+        else if (key == 'Enter') {
+            event.preventDefault()
+            calculate();
+        }
+
+        else if (key == 'Backspace') {
+            event.preventDefault();
+            deleteOn();
+        }
+
+        else if (key === 'Escape' || key === 'c'  || key === 'C') {
+            clearScreen();
+        }
+
+        else if (key === '.') {
+            appendToDisplay(key);
+        }
+    });
+})
+
